84091e6681b22bea191a5a1f5aa23b2d2c3a7e77
Harnoor Kaur <161191385+harnoor8616@users.noreply.github.com>
2025-06-09 19:38:46 +0530

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,36 @@
+# print() function
+print("Harnoor Kaur")
+print("Harnoor", "Kaur")
+
+# REPL stands for Read Evaluate Print Loop
+# in terminal.....Python + Enter
+# To open REPL
+
+# sep keyword
+print("Hello","World","I","am","learning","Python", sep=",")
+
+# end keyword
+print("Hello", end="...")
+print("World")
+
+# to get use input using input() function
+name=input("Please Enter your name:")
+print("Welcome",name)
+
+# creating a variable
+# variable_name = Variable value
+age=input("Please enter your age:")
+print("You are ",age, " years old.")
+
+# id() function
+# used to print address
+a=10
+b=10
+c="Harnoor"
+d=0.0000000000000000023
+e=0.0000000000000000023
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
+print(id(e))
